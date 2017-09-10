23c727a0324cfbba85410941ef31713d8e9396e7
Emil Plynning <emil.plynning@gmail.com>
2017-09-10 19:16:23 +0200

    Create small testing area and add new tree assets etc

--- Assets/Scripts/Camera/CameraMovementPrototype1.cs
+++ Assets/Scripts/Camera/CameraMovementPrototype1.cs
@@ -7,33 +7,32 @@ public class CameraMovementPrototype1 : MonoBehaviour {
 	public Camera cam;
 	private Ray currentFacing;
 	private Vector3 newPosition;
+    private float speedFactor = 0.5f;
 
 	void Update () {
 		newPosition = transform.position;
+		Quaternion q = transform.rotation;
 		Quaternion qNew = transform.rotation;
-		if (Input.GetMouseButton (1)) {
-			Quaternion q = transform.rotation;
-			currentFacing = cam.ScreenPointToRay (Input.mousePosition);
-			q.SetLookRotation (currentFacing.direction, new Vector3 (0, 1, 0));
-			qNew = Quaternion.Slerp (qNew, q, Time.deltaTime);
-		}
+		currentFacing = cam.ScreenPointToRay (Input.mousePosition);
+		q.SetLookRotation (currentFacing.direction, new Vector3 (0, 1, 0));
+		qNew = Quaternion.Slerp (qNew, q, Time.deltaTime);
 			
 		if (Input.GetKey (KeyCode.W)) {
-			newPosition += currentFacing.direction;
+			newPosition += currentFacing.direction * speedFactor;
 		}
 		if (Input.GetKey (KeyCode.S)) {
-			newPosition -= currentFacing.direction;
+			newPosition -= currentFacing.direction * speedFactor;
 		}
 
 		Vector3 v;
 
 		if (Input.GetKey (KeyCode.A)) {
 			v = transform.TransformDirection( new Vector3 (-1, 0, 0));
-			newPosition += v;
+			newPosition += v * speedFactor;
 		} 
 		if (Input.GetKey (KeyCode.D)) {
 			v = transform.TransformDirection( new Vector3 (1, 0, 0));
-			newPosition += v;
+			newPosition += v * speedFactor;
 		}
 
 		transform.SetPositionAndRotation (newPosition, qNew);
