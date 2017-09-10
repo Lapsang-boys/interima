1ff320919b045f15ef38c4ee5bb3d7bce3c725b5
Henry Eklind <henrye@kth.se>
2017-09-10 23:47:39 +0200

    Add BallOfDeath projectile launcher

---
+++ Assets/Scripts/SpellSystem/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	GameObject go; // The body that will be sent in the trajectory.
+	float ttl; // Time-to-live. Projectile will be garbage collected after ttl have elapsed.
+
+	public Projectile(GameObject go, float ttl) {
+		this.go = go;
+		this.ttl = ttl;
+	}
+
+	public void Launch(Vector3 towards, float thrust) {
+		Debug.Log ("Launching!");
+		// go.GetComponent<Rigidbody>().AddForce (towards * thrust);
+		go.GetComponent<Rigidbody>().velocity = towards * 10;
+	}
+
+	public void OnCollisionEnter(Collision collision) {
+		Debug.Log ("Hit the ground!");
+		Destroy (this);
+	}
+}
