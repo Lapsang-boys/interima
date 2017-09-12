79762c2d9fcbb0f7f2685b8b40cdfe5b4ada1558
Emil Plynning <emil.plynning@gmail.com>
2017-09-13 01:22:34 +0200

    Add collision for ball of death and add particle assets

---
+++ Assets/Scripts/SpellSystem/SpellInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellInfo : MonoBehaviour {
+    // ###################################################################################################
+    // This class is used to pass all neccecary info for interaction between a spell and it targets/caster
+    // ###################################################################################################
+
+    // ---------------------------------------------------------------------------------------------------
+    // Player objects
+    // ---------------------------------------------------------------------------------------------------
+
+    // GameObject that casted the spell, can be used to find position at impact etc
+    public GameObject caster;
+    // GameObject that was targeted by the spell, can be used to continiously update a homing missile etc
+    public GameObject target;
+
+    // ---------------------------------------------------------------------------------------------------
+    // Positions
+    // ---------------------------------------------------------------------------------------------------
+    
+    // Use this for the poistion of the caster at the time of casting
+    public Vector3 casterPosition;
+    // Use this for the origin of the spell/projectile (for instance if it is offset from the caster)
+    public Vector3 spellOrigin;
+    // Use this for the target point of the spell
+    public Vector3 spellTargetPoint;
+
+    // ---------------------------------------------------------------------------------------------------
+    // Spell data
+    // ---------------------------------------------------------------------------------------------------
+    
+    // --- Damage ---
+    // True = friends and self will be damaged / affected
+    public bool friendlyFire;
+    // Damage
+    public float damage;
+    // Area of effect
+    public float radius;
+    // DoT damage (amount of damage each time dot ticks)
+    public float dotDamage;
+    // DoT interval (how often the DoT amount will be applied) [Seconds between each tick]
+    public float dotInterval;
+    // Dot duration (how many seconds the DoT will persist)
+    public float dotDuration;
+
+    // --- Effects (slow etc) ---
+    // The effect applied, for instance ["slow"]
+    public string effect;
+    // Effect duration (how many seconds the effect will persist)
+    public float effectDuration;
+    // Effect amount percent, for instance [-30] for -30% slow
+    public float effectAmountPercent;
+    // Effect amount value, for instance [-30] for -30 attack or -30 movementspeed
+    public float effectAmountValue;
+}
---
+++ Assets/Scripts/Spells/ClassX/BallOfDeath/BallOfDeath.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallOfDeath : MonoBehaviour {
+	Vector3 position;
+	public GameObject caster;
+    private Camera cam;
+    private GameObject projectileBody;
+    public GameObject projectilePrefab;
+
+    private void Awake() {
+        // Camera used to get ray
+        cam = caster.GetComponentInChildren<Camera>();
+    }
+
+    public void FireProjectile() {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        // Postition derived from caster and direction derived from ray
+        position = caster.transform.position;
+        Vector3 direction = Vector3.Normalize(ray.direction);
+
+        // Create a GameObject (the prefab assigned in the inspector) 
+        projectileBody = (GameObject)Instantiate(projectilePrefab, position, Quaternion.identity);
+
+        // Attatch a spell info to the projectile
+        SpellInfo si = projectileBody.AddComponent<SpellInfo>();
+        // --- Add info needed for handeling the spell after launch (collisions etc) ---
+        si.caster = caster;
+        // ---
+        Debug.Log(si);
+
+        // Apply force to the rigid body of the game object
+        Rigidbody rb = projectileBody.GetComponent<Rigidbody>();
+        rb.position += direction * 2;
+        rb.AddForce(direction * 1000);
+    }
+		
+    void Update () {
+        // Listen for input (left mouse button)
+		if (Input.GetMouseButtonDown(0)) {
+			FireProjectile ();
+		}
+	}
+}
